f4dc7c485592a7450038a98213fe5fef971e91d2
1-8192 <allegranzia@gmail.com>
2019-05-02 15:06:16 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Recipe.destroy_all
+Review.destroy_all
+Ingredient.destroy_all
+RecipesIngredient.destroy_all
