5dd58f9654724037c80d8e320fce742b50e0b348
mnkillebr <killebrew.biz@gmail.com>
2019-05-03 14:30:12 -0400

    working on auth

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,20 @@
 class Api::V1::UsersController < ApplicationController
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      render json: {user: UserSerializer.new(@user)}, status: :created
+    else
+      render json: {error: 'Failed to create user'}, status: :not_acceptable
+    end
+  end
+
+  
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :recipes, only: [:index] 
+
+  namespace :api do
+    namespace :v1 do
+      resources :recipes, only: [:index]
+      resources :users, only: [:create]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+    end
+  end
+
+  
 
 end
