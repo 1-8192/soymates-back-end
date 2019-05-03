5a17d4418df898f67aad97462dcd2839c4dfc906
1-8192 <allegranzia@gmail.com>
2019-05-03 14:15:02 -0400

    added password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :reviews
   has_many :recipes, through: :reviews
+
+  validates :username, uniqueness: { case_sensitive: false }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_02_183351) do
+ActiveRecord::Schema.define(version: 2019_05_03_175853) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2019_05_02_183351) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
