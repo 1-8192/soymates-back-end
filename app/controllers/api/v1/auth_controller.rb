8c07b9b608bd8045adc875e432916c409cc5fca8
1-8192 <allegranzia@gmail.com>
2019-05-08 16:01:38 -0400

    more merging

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -5,7 +5,6 @@ class Api::V1::AuthController < ApplicationController
     @user = User.find_by(email: user_login_params[:email])
     if @user && @user.authenticate(user_login_params[:password])
       token = encode_token({user_id: @user.id})
-      render json: {user: UserSerializer.new(@user), jwt: token}, status: :accepted
       render json: {user: UserSerializer.new(@user), jwt: token, reviews:@user.reviews}, status: :accepted
     else
       render json: {message: 'Invalid Username or Password'}, status: :unauthorized
